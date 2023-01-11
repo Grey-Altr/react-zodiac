8793c3269456895a22ca517134e1adb296b2371b
Grey Altr <grey.sifr@gmail.com>
2023-01-10 17:18:12 -0800

    completed Zodiac.js

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { zodiac } from '../../data.js';
-import zodiacData from '../zodiac/Zodiac.js';
+import { zodiac } from './data.js';
+import ZodiacData from '../zodiac/Zodiac.js';
 import background from '../../public/background.jpeg';
 import './Main.css';
 
@@ -8,7 +8,7 @@ export default function Main() {
   return (
     <main style={{ background: `url(${background})` }}>
       {zodiac.map((item) => (
-        <zodiacData key={item.name} {...item} />
+        <ZodiacData key={item.name} {...item} />
       ))}
     </main>
   );
