0397b4f8b4c221f01e9576d497c03dd1e02fef37
Grey Altr <grey.sifr@gmail.com>
2023-01-10 17:24:15 -0800

    npm deploy: success

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { zodiac } from './data.js';
 import ZodiacData from '../zodiac/Zodiac.js';
-import background from '../../public/background.jpeg';
+import background from '../../background.jpeg';
 import './Main.css';
 
 export default function Main() {
